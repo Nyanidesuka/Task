1a48df3a2fdbc49eea40f6e5a894fa827cedac52
Haley Jones <nyanidesuka@icloud.com>
2019-05-10 08:36:11 -0600

    Initial Commit

---
+++ Task/Models/DatesHelper.swift
@@ -0,0 +1,9 @@
+//
+//  DatesHelper.swift
+//  Task
+//
+//  Created by Haley Jones on 5/8/19.
+//  Copyright © 2019 HaleyJones. All rights reserved.
+//
+
+import Foundation
---
+++ Task/Views/TaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewCell.swift
+//  Task
+//
+//  Created by Haley Jones on 5/8/19.
+//  Copyright © 2019 HaleyJones. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
